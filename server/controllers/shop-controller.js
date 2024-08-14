d07f07c6244085248bdac5b49eae9e932ce74304
marcellowoods <dankkmemes@gmail.com>
2024-08-14 12:42:40 +0300

    querying

--- server/controllers/shop-controller.js
+++ server/controllers/shop-controller.js
@@ -6,50 +6,9 @@
 // } from 'http-status-codes';
 
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+const { Op } = require('sequelize');
 
-
-const Product = require('../models/product');
-
-const filterProductsByPrice = (products, minMaxValues) => {
-
-    const min = minMaxValues[0];
-    const max = minMaxValues[1];
-
-
-    return products.filter((product) => {
-
-        return product.price >= min && product.price <= max;
-
-    })
-
-
-
-}
-
-const filterProductsByCategory = (products, value) => products.filter((p) => p.category == value);
-
-const filterProductsByBrand = (products, brands) => products.filter((p) => p.brand && brands.includes(p.brand));
-
-const filterProducts = (products, field, value) => {
-
-    // console.log(typeof value);
-
-    return products.filter((product) => {
-
-        let prop = product[field];
-
-        //some products don't have brand
-        if (!prop) {
-            prop = "";
-        }
-
-        return prop.toString().toLowerCase().includes(value.toLowerCase());
-
-    })
-
-
-
-}
+const Product = require("../models/product")
 
 const getMinMaxPrice = (products) => {
 
@@ -70,121 +29,6 @@ const getMinMaxPrice = (products) => {
 
     return { min, max, stepSize };
 }
-
-const getCategoriesList = (products) => {
-
-    let categoriesList = Array.from(new Set(products.map(p => p.category)));
-
-    return categoriesList;
-}
-
-const getBrandsList = (products) => {
-
-    let brandsList = Array.from(new Set(products.map(p => p.brand)));
-
-    return brandsList;
-}
-
-
-const createSortComparator = (sortField, desc) => {
-
-
-    if (!desc) {
-
-        return (a, b) => {
-
-            return a[sortField] - b[sortField];
-
-        }
-
-    } else {
-
-        return (a, b) => {
-
-            return b[sortField] - a[sortField]
-
-        }
-
-    }
-
-}
-
-const getFilteredDataForField = (data, field, value) => {
-
-    switch (field) {
-        case 'price':
-
-            return filterProductsByPrice(data, value);
-
-        case 'category':
-
-            return filterProductsByCategory(data, value);
-
-        case 'brand':
-
-            return filterProductsByBrand(data, value);
-
-        default:
-
-            return filterProducts(data, field, value);
-
-    }
-}
-
-const getFilteredDataForGlobalFilter = (data, globalFilterString) => {
-
-    return data.filter(p => {
-
-        for (let field in p) {
-
-            let prop = p[field];
-            if (!prop) {
-                return false;
-            }
-
-            if (prop.toString().toLowerCase().includes(globalFilterString.toLowerCase())) {
-                return true;
-            }
-
-        }
-
-        return false;
-    })
-}
-
-//apply all filters
-const getFilteredDataForEveryField = (data, filters) => {
-
-    let filteredData = data;
-
-    filters.forEach((filter) => {
-
-        const { id: field, value } = filter;
-        filteredData = getFilteredDataForField(filteredData, field, value);
-
-    })
-
-    return filteredData;
-}
-
-const getFilteredDataForEveryFieldExceptOne = (data, filters, dontFilterField) => {
-
-    let filteredData = data;
-
-    filters.forEach((filter) => {
-
-        const { id: field, value } = filter;
-        if (field == dontFilterField) {
-            return;
-        }
-        filteredData = getFilteredDataForField(filteredData, field, value);
-    })
-
-    return filteredData;
-
-
-}
-
 
 exports.getProducts = async (req, res, next) => {
 
@@ -212,74 +56,74 @@ exports.getProducts = async (req, res, next) => {
     // Add pagination
     // Add filtering
 
-    let unfilteredData = await Product.findAll();
-
-    let filteredDataWithGlobalFilter = getFilteredDataForGlobalFilter(unfilteredData, globalFilter);
-
-    let filteredDataForAllFields = getFilteredDataForEveryField(unfilteredData, filters);
-    
-    filteredDataForAllFields = getFilteredDataForGlobalFilter(filteredDataForAllFields, globalFilter);
-
-    let meta = {};
-
-
-    const priceFilter = filters.find((filter) => filter.id == "price");
-
-    if (priceFilter) {
-
-        let filteredData = getFilteredDataForEveryFieldExceptOne(filteredDataWithGlobalFilter, filters, "price");
-        const minMaxPrice = getMinMaxPrice(filteredData);
-        meta.minMaxPrice = minMaxPrice;
-
-    }else{
-
-        const minMaxPrice = getMinMaxPrice(filteredDataForAllFields);
-        meta.minMaxPrice = minMaxPrice;
-    }
-
+    const brandsFilter = filters.find((filter) => filter.id == "brand");
     const categoryFilter = filters.find((filter) => filter.id == "category");
+    const titleFilter = filters.find((filter) => filter.id == "title");
 
+    const titleFilterString = titleFilter ? titleFilter.value : "";
+    const categoryFilterString = categoryFilter ? categoryFilter.value : "";
+    const brandsFilterArray = brandsFilter ? brandsFilter.value : [];
+
+    const filterClauese = {};
     if (categoryFilter) {
 
-        let filteredData = getFilteredDataForEveryFieldExceptOne(filteredDataWithGlobalFilter, filters, "category");
-        const categoriesList = getCategoriesList(filteredData);
-        meta.categoriesList = categoriesList;
-
-    }else{
-        const categoriesList = getCategoriesList(filteredDataForAllFields);
-        meta.categoriesList = categoriesList;
-
+        filterClauese.category = {
+            [Op.eq]: categoryFilterString,
+        }
     }
-
-    const brandsFilter = filters.find((filter) => filter.id == "brand");
 
     if (brandsFilter) {
 
-        let filteredData = getFilteredDataForEveryFieldExceptOne(filteredDataWithGlobalFilter, filters, "brand");
-        const brandsList = getBrandsList(filteredData);
-        meta.brandsList = brandsList;
-
-    }else{
-
-        const brandsList = getBrandsList(filteredDataForAllFields);
-        meta.brandsList = brandsList;
+        filterClauese.brand = { [Op.in]: brandsFilterArray }
     }
 
-    
+    let data = await Product.findAll({
+        where: {
+            [Op.or]: {
+                title: {
+                    [Op.like]: '%' + globalFilter + '%',
+                },
+                category: {
+                    [Op.like]: '%' + globalFilter + '%'
+                },
+                brand: {
+                    [Op.like]: '%' + globalFilter + '%'
+                },
 
-    let data = filteredDataForAllFields;
-    const totalRowCount = data.length;
-    meta.totalRowCount = totalRowCount;
+            },
+            [Op.and]: {
 
-    if (sorting[0]) {
+                title: {
+                    [Op.like]: '%' + titleFilterString + '%',
+                },
 
-        const { id: sortField, desc } = sorting[0];
-        const sortComparator = createSortComparator(sortField, desc);
-        data = data.sort(sortComparator);
+                ...filterClauese
 
-    }
+            },
+
+        },
+    });
 
     data = data.slice(start, start + size);
+
+    let meta = {}
+
+    const minMaxPrice = getMinMaxPrice(data);
+    meta.minMaxPrice = minMaxPrice;
+
+    meta.categoriesList = await Product.findAll({
+        attributes: ['category'],
+        group: ['category']
+    }).then(product =>
+        product.map(product => product.category)
+    );
+
+    meta.brandsList = await Product.findAll({
+        attributes: ['brand'],
+        group: ['brand']
+    }).then(product =>
+        product.map(product => product.brand)
+    );
 
     const response = {
         data,
