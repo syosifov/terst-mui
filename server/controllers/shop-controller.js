cfa1ae0ab358639a56056d2f6fbfb42e51665b02
marcellowoods <dankkmemes@gmail.com>
2024-08-14 14:15:37 +0300

    pagination and filtering

--- server/controllers/shop-controller.js
+++ server/controllers/shop-controller.js
@@ -6,7 +6,7 @@
 // } from 'http-status-codes';
 
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
-const { Op } = require('sequelize');
+const { Op, where } = require('sequelize');
 
 const Product = require("../models/product")
 
@@ -28,6 +28,59 @@ const getMinMaxPrice = (products) => {
 
 
     return { min, max, stepSize };
+}
+
+const createWhereClause = (filters, globalFilter) => {
+
+    const brandsFilter = filters.find((filter) => filter.id == "brand");
+    const categoryFilter = filters.find((filter) => filter.id == "category");
+    const titleFilter = filters.find((filter) => filter.id == "title");
+
+    const titleFilterString = titleFilter ? titleFilter.value : "";
+    const categoryFilterString = categoryFilter ? categoryFilter.value : "";
+    const brandsFilterArray = brandsFilter ? brandsFilter.value : [];
+
+    const filterClauese = {};
+    if (categoryFilter) {
+
+        filterClauese.category = {
+            [Op.eq]: categoryFilterString,
+        }
+    }
+
+    if (brandsFilter) {
+
+        filterClauese.brand = { [Op.in]: brandsFilterArray }
+    }
+
+    const whereClause = {
+        [Op.or]: {
+            title: {
+                [Op.like]: '%' + globalFilter + '%',
+            },
+            category: {
+                [Op.like]: '%' + globalFilter + '%'
+            },
+            brand: {
+                [Op.like]: '%' + globalFilter + '%'
+            },
+
+        },
+        [Op.and]: {
+
+            title: {
+                [Op.like]: '%' + titleFilterString + '%',
+            },
+
+            ...filterClauese
+
+        },
+
+    }
+
+    return whereClause
+
+
 }
 
 exports.getProducts = async (req, res, next) => {
@@ -52,73 +105,48 @@ exports.getProducts = async (req, res, next) => {
 
     // console.log(data);
     // TODO:
-    // Add sorting
-    // Add pagination
-    // Add filtering
+    // add sorting
+    // add price min max filter
 
-    const brandsFilter = filters.find((filter) => filter.id == "brand");
-    const categoryFilter = filters.find((filter) => filter.id == "category");
-    const titleFilter = filters.find((filter) => filter.id == "title");
-
-    const titleFilterString = titleFilter ? titleFilter.value : "";
-    const categoryFilterString = categoryFilter ? categoryFilter.value : "";
-    const brandsFilterArray = brandsFilter ? brandsFilter.value : [];
-
-    const filterClauese = {};
-    if (categoryFilter) {
-
-        filterClauese.category = {
-            [Op.eq]: categoryFilterString,
-        }
-    }
-
-    if (brandsFilter) {
-
-        filterClauese.brand = { [Op.in]: brandsFilterArray }
-    }
+    const whereClause = createWhereClause(filters, globalFilter);
 
     let data = await Product.findAll({
-        where: {
-            [Op.or]: {
-                title: {
-                    [Op.like]: '%' + globalFilter + '%',
-                },
-                category: {
-                    [Op.like]: '%' + globalFilter + '%'
-                },
-                brand: {
-                    [Op.like]: '%' + globalFilter + '%'
-                },
+        where: whereClause,
+        offset: start,
+        limit: size
+    }
+    );
 
-            },
-            [Op.and]: {
 
-                title: {
-                    [Op.like]: '%' + titleFilterString + '%',
-                },
+    const totalRowCount = await Product.count({
+        where: whereClause,
+    })
 
-                ...filterClauese
-
-            },
-
-        },
-    });
-
-    data = data.slice(start, start + size);
+    // data = data.slice(start, start + size);
 
     let meta = {}
+
+    meta.totalRowCount = totalRowCount;
 
     const minMaxPrice = getMinMaxPrice(data);
     meta.minMaxPrice = minMaxPrice;
 
+    const categoriesWhereClause = createWhereClause(filters, globalFilter);
+    delete categoriesWhereClause[Op.and]["category"];
+
     meta.categoriesList = await Product.findAll({
+        where: categoriesWhereClause,
         attributes: ['category'],
         group: ['category']
     }).then(product =>
         product.map(product => product.category)
     );
 
+    const brandsWhereClause = createWhereClause(filters, globalFilter);
+    delete brandsWhereClause[Op.and]["brand"];
+
     meta.brandsList = await Product.findAll({
+        where: brandsWhereClause,
         attributes: ['brand'],
         group: ['brand']
     }).then(product =>
