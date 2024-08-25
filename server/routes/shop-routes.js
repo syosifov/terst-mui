c2d5a8d390d7d333f801efdf9531e90f8cd38797
marcellowoods <dankkmemes@gmail.com>
2024-08-25 17:14:40 +0300

    stripe payment

--- server/routes/shop-routes.js
+++ server/routes/shop-routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const shopController = require('../controllers/shop-controller');
-const cartController = require('../controllers/cart-controller');
+const paymentRoutes = require('./payment-routes');
 
 const router = express.Router();
 
 router.get('/products', shopController.getProducts);
 router.get('/products-by-id', shopController.getProductsById);
-router.post('/checkout', cartController.checkout);
+router.use('/payment', paymentRoutes);
 
 module.exports = router;
