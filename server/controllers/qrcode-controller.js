8f0cd22b6e5427065e4aae035b472f1bbefd575f
marcellowoods <dankkmemes@gmail.com>
2024-08-30 12:24:49 +0300

    qr codes

---
+++ server/controllers/qrcode-controller.js
@@ -0,0 +1,12 @@
+const QRCode = require('qrcode');
+
+exports.generateQR = async (req, res) => {
+    try {
+        const url = req.query.url || 'https://example.com';
+        const qrCodeImage = await QRCode.toDataURL(url);
+        res.send(`<img src="${qrCodeImage}" alt="QR Code"/>`);
+    } catch (err) {
+        console.error('Error generating QR code:', err);
+        res.status(500).send('Internal Server Error');
+    }
+};
--- server/routes/shop-routes.js
+++ server/routes/shop-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const shopController = require('../controllers/shop-controller');
 const paymentRoutes = require('./payment-routes');
+const qrcodeController = require('../controllers/qrcode-controller');
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.get('/products', shopController.getProducts);
 router.get('/products-by-id', shopController.getProductsById);
 router.post('/bot/message', shopController.botMessage)
 router.use('/payment', paymentRoutes);
+router.get('/generate-qr', qrcodeController.generateQR);
 
 module.exports = router;
